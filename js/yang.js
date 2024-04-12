99e426584b49a70c616792bafcd565e9292f9c94
BetterAndBetterII <123090848@link.cuhk.edu.cn>
2024-04-12 15:58:20 +0800

    update readme

--- js/yang.js
+++ js/yang.js
@@ -3,11 +3,11 @@ function calculateYang(y_l, y_a_, y_b, y_d, y_a_a) {
     const y_m = 9.3;
     const y_g = 9.7915;
 
-    const y_ii = Math.pow(y_l - y_a_, 2);
+    const y_ii = math.pow(y_l - y_a_, 2);
     console.log(`y_ii: ${y_ii}`)
     const y_fenzi = 12 * y_m * y_g * y_ii * 1000;
     console.log(`y_fenzi: ${y_fenzi}`)
-    const y_d_cubed = Math.pow(y_d, 3);
+    const y_d_cubed = math.pow(y_d, 3);
     console.log(`y_d_cubed: ${y_d_cubed}`)
     const y_fenmu = y_b * y_d_cubed;
     console.log(`y_fenmu: ${y_fenmu}`)
