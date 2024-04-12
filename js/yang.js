1e3a30156ccf8321d8567cdba2fc989cb8113882
HNU下柿 <154401236+xiaoshengyidian@users.noreply.github.com>
2024-04-12 23:20:55 +0800

    Update yang.js
    新增输入砝码数量，并修正了乘以0.5的步骤

--- js/yang.js
+++ js/yang.js
@@ -1,18 +1,21 @@
-function calculateYang(y_l, y_a_, y_b, y_d, y_a_a) {
-    console.log(`y_l: ${y_l}, y_a_: ${y_a_}, y_b: ${y_b}, y_d: ${y_d}, y_a_a: ${y_a_a}`)
+function calculateYang(y_l, y_a_, y_b, y_d, y_a_a,y_n) {
+    console.log(`y_l: ${y_l}, y_a_: ${y_a_}, y_b: ${y_b}, y_d: ${y_d}, y_a_a: ${y_a_a}, y_n:${y_n}`)//新增砝码个数
     const y_m = math.bignumber(9.3);
     const y_g = math.bignumber(9.7915);
     y_l = math.bignumber(y_l);
     y_a_ = math.bignumber(y_a_);
     y_b = math.bignumber(y_b);
     y_d = math.bignumber(y_d);
+    
 
     y_a_a = math.bignumber(y_a_a);
-    y_a_a = math.chain(y_a_a).multiply(0.5).done();
+    y_a_ = math.chain(y_a_).multiply(0.5).done();
 
     const y_ii = math.chain(y_l).subtract(y_a_).pow(2).done();
     console.log(`y_ii: ${y_ii}`)
-    const y_fenzi = math.chain(y_m).multiply(y_g).multiply(y_ii).multiply(12000).done();
+    
+    const y_fenzi = math.chain(y_m).multiply(y_g).multiply(y_n).multiply(y_ii).multiply(12000).done();
+    
     console.log(`y_fenzi: ${y_fenzi}`)
     const y_d_cubed = math.chain(y_d).pow(3).done();
     console.log(`y_d_cubed: ${y_d_cubed}`)
