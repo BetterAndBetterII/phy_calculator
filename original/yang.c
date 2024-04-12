8ec465457c30980a4a2ea205e88e1c2ae80dabf8
HNU下柿 <154401236+xiaoshengyidian@users.noreply.github.com>
2024-04-12 23:08:47 +0800

    Update yang.c
    增加输入砝码的个数

--- original/yang.c
+++ original/yang.c
@@ -15,29 +15,32 @@ int main() {
 	double y_b=0;
 	double y_d=0;
 	double y_E=0;
+	double y_n=0;
 	
-	printf("ÇëÊäÈël\n");
+	printf("请输入l\n");
 	scanf("%lf", &y_l);
-	printf("ÇëÊäÈëa/2 \n");
+	printf("请输入a/2 \n");
 	scanf("%lf", &y_a_);
-	printf("ÇëÊäÈëb\n");
+	printf("请输入b\n");
 	scanf("%lf", &y_b);
-	printf("ÇëÊäÈëd\n");
+	printf("请输入d\n");
 	scanf("%lf", &y_d);
 	//输入单个砝码质量
-	 printf("ÇëÊäÈëd\n");
+	printf("请输入m\n");
 	scanf("%lf", & y_m);
-
+	
+	printf("请输入砝码数量n\n");
+	scanf("%lf", & y_n);
 
 	double y_ii = y_l - y_a_;
 	y_ii = y_ii*y_ii;
-	double y_fenzi = 12 * y_m *5* y_g * y_ii * 1000;    //五个砝码
+	double y_fenzi = 12 * y_m *y_n* y_g * y_ii * 1000;    //n个砝码
 	y_d = y_d * y_d * y_d;
 	double y_fenmu=y_b*y_d;
 
 	/*while (1) {*/
 		double y_a_a=0;
-		printf("ÇëÊäÈëa_a\n");
+		printf("请输入2θ\n");
 		scanf("%lf", &y_a_a);
 		//
 		y_E = y_fenzi / (y_fenmu*y_a_a);
