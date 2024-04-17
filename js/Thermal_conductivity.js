b6e65d01ef0a680d31cb85f0f2dd74bd175b419a
HNU一下柿 <154401236+xiaoshengyidian@users.noreply.github.com>
2024-04-17 13:00:04 +0800

    Update Thermal_conductivity.js
    Bignumber

--- js/Thermal_conductivity.js
+++ js/Thermal_conductivity.js
@@ -13,6 +13,14 @@ let T2 = 0;
 function REDAO(B_l, A_d, A_D, A_m, k, T1, T2) {
     const PI = Math.acos(-1);
     const c = 896;
+    B_l =math.bignumber(B_l);
+    A_d =math.bignumber(A_d);
+    A_D =math.bignumber(A_D);
+    A_m =math.bignumber(A_m);
+    k =math.bignumber(k);
+    T1 =math.bignumber(T1);
+    T2 =math.bignumber(T2);
+    PI =math.bignumber(PI);
     let fenzi = 2 * c * (A_D + (4 * A_d)) * k * A_m*B_l;
     let fenmu = PI * A_D * A_D * (A_D + (2 * A_d)) * (T1 - T2);
     let lamu = (fenzi / fenmu) / 10;
