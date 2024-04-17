d7180e7d2c163ce5c9fa2dc37bb255b6f4fb6a0d
HNU一下柿 <154401236+xiaoshengyidian@users.noreply.github.com>
2024-04-17 12:33:10 +0800

    Update Thermal_conductivity.js

--- js/Thermal_conductivity.js
+++ js/Thermal_conductivity.js
@@ -11,6 +11,7 @@ let k = 0;
 let T1 = 0;
 let T2 = 0;
 function REDAO(B_l, A_d, A_D, A_m, k, T1, T2) {
+    const PI = Math.acos(-1);
     const c = 896;
     let fenzi = 2 * c * (A_D + (4 * A_d)) * k * A_m*B_l;
     let fenmu = PI * A_D * A_D * (A_D + (2 * A_d)) * (T1 - T2);
