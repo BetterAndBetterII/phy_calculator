de3b1e10bc04a67cc899e8c018371312039c057a
HNU一下柿 <154401236+xiaoshengyidian@users.noreply.github.com>
2024-04-17 22:32:48 +0800

    Update Thermal_conductivity.js
    正则匹配

--- js/Thermal_conductivity.js
+++ js/Thermal_conductivity.js
@@ -11,6 +11,8 @@ let T1 = 0;
 let T2 = 0;
 function REDAO(B_l, A_d, A_D, A_m, k, T1, T2) {
     const c = 896;
+    A_d= A_d.match(/\d+\.\d+/g);
+    A_D= A_D.match(/\d+\.\d+/g);
     B_l =math.bignumber(B_l);
     A_d =math.bignumber(A_d);
     A_D =math.bignumber(A_D);
@@ -18,6 +20,7 @@ function REDAO(B_l, A_d, A_D, A_m, k, T1, T2) {
     k =math.bignumber(k);
     T1 =math.bignumber(T1);
     T2 =math.bignumber(T2);
+    
     let fenzi = 2 * c * (A_D + (4 * A_d)) * k * A_m*B_l;
     let fenmu = PI * A_D * A_D * (A_D + (2 * A_d)) * (T1 - T2);
     let lamu = (fenzi / fenmu) / 10;
