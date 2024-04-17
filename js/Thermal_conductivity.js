d462a299f8fa0f9129b5f7968208416f1452176e
BetterAndBetterII <123090848@link.cuhk.edu.cn>
2024-04-17 15:51:20 +0800

    fix

--- js/Thermal_conductivity.js
+++ js/Thermal_conductivity.js
@@ -1,7 +1,6 @@
 
 
-
-const PI = Math.acos(-1);
+const PI =math.bignumber(math.acos(-1));
 let B_l = 0;
 let A_d = 0;
 let A_D = 0;
@@ -11,7 +10,6 @@ let k = 0;
 let T1 = 0;
 let T2 = 0;
 function REDAO(B_l, A_d, A_D, A_m, k, T1, T2) {
-    const PI = Math.acos(-1);
     const c = 896;
     B_l =math.bignumber(B_l);
     A_d =math.bignumber(A_d);
@@ -20,7 +18,6 @@ function REDAO(B_l, A_d, A_D, A_m, k, T1, T2) {
     k =math.bignumber(k);
     T1 =math.bignumber(T1);
     T2 =math.bignumber(T2);
-    PI =math.bignumber(PI);
     let fenzi = 2 * c * (A_D + (4 * A_d)) * k * A_m*B_l;
     let fenmu = PI * A_D * A_D * (A_D + (2 * A_d)) * (T1 - T2);
     let lamu = (fenzi / fenmu) / 10;
