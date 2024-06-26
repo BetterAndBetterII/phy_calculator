8136aeb36fc3d8c21ef331c6ea95a8d879b17191
HNU一下柿 <154401236+xiaoshengyidian@users.noreply.github.com>
2024-06-26 11:04:20 +0800

    Update 计算平均转动惯量.c
    增加理论转动惯量的计算

--- 第八次物理实验数据分析/c语言库/计算平均转动惯量.c
+++ 第八次物理实验数据分析/c语言库/计算平均转动惯量.c
@@ -5,13 +5,15 @@
 
 double rotational_inertia_calculate(double R_I_m,double R_I_R,double R_I_r,double R_I_T,double R_I_L);//计算转动惯量
 //注意单位？！！！
+double rotational_inertia_in_theory_calculate(double R_T_M,double R_T_R);
 int main() {
 	double g = 0;//重力加速度（单位）
 
 	double m_a = 0;//圆盘a的质量（g）
 	double T_a = 0;//圆盘a的扭摆周期（s）
 	double J_a = 0;//圆盘a的平均转动惯量
-
+	double Th_J_a=0;//理论转动惯量
+	
 	//意义未明确的物理量
 	double Ra = 0;//(cm)
 	double Rb = 0;//(cm)
@@ -26,11 +28,13 @@ int main() {
 
 	double T_ab = 0;//圆盘a和b的扭摆周期（s）
 	double J_ab = 0;//圆盘a和b的平均转动惯量
+	double Th_J_b=0;//理论转动惯量
 	
 	J_a=rotational_inertia_calculate(m_a,R,r,T_a,L);
 	J_ab=rotational_inertia_calculate(m_a+m_b,R,r,T_ab,L);
 
-
+	Th_J_a=rotational_inertia_in_theory_calculate(m_a,Ra);
+	Th_J_b=rotational_inertia_in_theory_calculate(m_b,Rb);
 
 }
 double rotational_inertia_calculate(double R_I_m, double R_I_R, double R_I_r, double R_I_T, double R_I_L) {
@@ -51,3 +55,16 @@ double rotational_inertia_calculate(double R_I_m, double R_I_R, double R_I_r, do
 	res = fenzi / fenmu;
 	return res;
 }
+
+
+
+double rotational_inertia_in_theory_calculate(double R_T_M,double R_T_R){
+	//单位转化
+	R_T_M=R_T_M*0.001;
+	R_T_R=R_T_R*0.01;
+
+	double res=0.5*R_T_M*R_T_R*R_T_R;
+	return res;
+}
+
+
